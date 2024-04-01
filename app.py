502f9694f441c6c1770b6aa9e413a5673467a610
izumiaa <91410858+izumiaa@users.noreply.github.com>
2024-04-01 21:17:04 +0800

    add topic model to app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 
 from helper_functions.classifier import classify_text
+from helper_functions.topic_model import get_topic, generate_wordcloud_per_topic
 
 # Define the Streamlit app
 def main():
@@ -11,7 +12,7 @@ def main():
     # Create a text input field for the user
     user_input = st.text_input("Enter your text here:")
 
-    # Add a button to run the classification
+    # Add a button to run the classification and topic modeling
     if st.button("Classify"):
         if user_input:
             classification_result = classify_text(user_input, model_path="./models/classfication/best_lstm.h5")
@@ -21,10 +22,19 @@ def main():
                 st.markdown("<p style='font-size: 36px; color: red;'>Scam!</p>", unsafe_allow_html=True)
             else:
                 st.markdown("<p style='font-size: 36px; color: green;'>Not scam!</p>", unsafe_allow_html=True)
+
+            # Call the get_topic function to extract the topic and coherence score
+            path = '../../data/train_data.csv'
+            topic_chosen_dict, topic_label, topic_coher_score = get_topic(path, user_input)
+
+            # Display the coherence score
+            st.write(f"The coherence score of your text with the topic chosen is {round(topic_coher_score, 4)}")
+
+            # Generate and display the word cloud
+            st.markdown("<p style='font-size: 24px; margin-top: 20px;'>Word Cloud for Chosen Topic:</p>", unsafe_allow_html=True)
+            generate_wordcloud_per_topic(topic_chosen_dict, topic_label)
         else:
             st.warning("Please enter some text.")
-
-
 
 # Run the Streamlit app
 if __name__ == "__main__":
---
+++ helper_functions/topic_model.py
@@ -0,0 +1,182 @@
+import os
+import re
+import ast
+import pandas as pd
+import numpy as np
+import tqdm
+import gensim
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import sent_tokenize
+
+from gensim.models import Phrases
+from gensim import corpora
+from gensim.models import CoherenceModel
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt 
+from gensim.models.nmf import Nmf
+
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+def preprocess_text(text):
+
+    # Define custom stopwords
+    custom_stopwords = set(stopwords.words('english'))
+    # custom_stopwords = set(stopwords.words('english')) | {'enron'}
+
+    # Initialize WordNet lemmatizer
+    lemmatizer = WordNetLemmatizer()
+
+    if not isinstance(text, str):
+        return ""
+    # Remove non-alphabetical characters
+    text = re.sub(r'[^a-zA-Z]', ' ', text)
+    # Tokenize into words, remove stopwords, lemmatize, and filter out short words
+    tokens = word_tokenize(text.lower())
+    filtered_words = [lemmatizer.lemmatize(word) for word in tokens if word not in custom_stopwords and len(word) > 1]
+    return filtered_words
+
+def get_dict(path): 
+    # Read the CSV file
+    df = pd.read_csv(path)
+
+    # Preprocess text data
+    df['tokens'] = df['text'].apply(preprocess_text)
+
+    # Separate spam and ham text - Unigram
+    uni_spam_df = df[df['label'] == 'spam']
+    uni_ham_df = df[df['label'] == 'ham']
+
+    # Create dictionary and corpus for spam text - Unigram
+    uni_spam_dictionary = corpora.Dictionary(uni_spam_df['tokens'])
+    uni_spam_corpus = [uni_spam_dictionary.doc2bow(text) for text in uni_spam_df['tokens']]
+
+    # Create dictionary and corpus for ham text - Unigram
+    uni_ham_dictionary = corpora.Dictionary(uni_ham_df['tokens'])
+    uni_ham_corpus = [uni_ham_dictionary.doc2bow(text) for text in uni_ham_df['tokens']]
+
+    # Text Col
+    uni_col = "tokens"
+
+    return uni_spam_df, uni_ham_df, uni_spam_corpus, uni_ham_corpus, uni_spam_dictionary, uni_ham_dictionary
+
+# Build NMF model for spam text
+def spamNMFModel(spamDict, hamDict, spamCorp, hamCorp, spamIdDict, hamIdDict, textCol, numTopics):
+    
+    spam_nmf_model = Nmf(
+        corpus=spamCorp, 
+        num_topics=numTopics, 
+        id2word=spamIdDict,
+        chunksize=100,
+        passes=2,
+        random_state=42
+    )
+
+    # Build LDA model for ham text
+    ham_nmf_model = Nmf(corpus=hamCorp, num_topics=numTopics, id2word=hamIdDict)
+        
+    # Calculate coherence score for spam topics
+    spam_coherence_model = CoherenceModel(model=spam_nmf_model, texts=spamDict[textCol], dictionary=spamIdDict, coherence='c_v')
+    spam_coherence_score = spam_coherence_model.get_coherence()
+#     print("Coherence score for spam topics:", spam_coherence_score)
+
+    # Calculate coherence score for ham topics
+    ham_coherence_model = CoherenceModel(model=ham_nmf_model, texts=hamDict[textCol], dictionary=hamIdDict, coherence='c_v')
+    ham_coherence_score = ham_coherence_model.get_coherence()
+#     print("Coherence score for ham topics:", ham_coherence_score)
+    
+    return spam_nmf_model, ham_nmf_model, spam_coherence_score, ham_coherence_score
+
+# def get_gensim_nmf_model(uni_spam_df, uni_ham_df, uni_spam_corpus, uni_ham_corpus, uni_spam_dictionary, uni_ham_dictionary): 
+
+#     # Gensim NMF Model - Uni - Spam
+#     uni_spam_nmf_num_topics1 = 7
+#     uni_spam_nmf_model1, uni_ham_nmf_model1, uni_spam_nmf_coher1, uni_ham_nmf_coher1 = spamNMFModel(uni_spam_df, uni_ham_df, uni_spam_corpus, uni_ham_corpus, uni_spam_dictionary, uni_ham_dictionary, "tokens", uni_spam_nmf_num_topics1)
+
+#     new_topics=[]
+
+#     for topic_id, topic in uni_spam_nmf_model1.print_topics(num_topics=uni_spam_nmf_num_topics1, num_words=20):
+#         # print(f'Topic {topic_id}: {topic}')
+#         indiv_topic_dict = {}
+#         indiv_topic = topic.split(" + ")
+#         for ind in indiv_topic:
+#             sep = ind.split("*")
+#             word = sep[1][1:-1]
+#             val = float(sep[0])
+#             indiv_topic_dict[word] = val
+#         new_topics.append(indiv_topic_dict)
+
+#     print("Coherence score for spam topic model:", uni_spam_nmf_coher1)
+#     return uni_spam_nmf_model1, new_topics
+
+
+
+def get_topic(path, input_text): 
+    inputText = preprocess_text(input_text)
+    uni_spam_df, uni_ham_df, uni_spam_corpus, uni_ham_corpus, uni_spam_dictionary, uni_ham_dictionary = get_dict(path) 
+    inputTextDf = pd.DataFrame({"text": inputText})
+
+    min_topics = 2
+    max_topics = 11
+    step_size = 1
+    topics_range = range(min_topics, max_topics, step_size)
+
+    topic_labels_spam = [
+        "HTML/Website Spams",
+        "Email/Phishing Scams",
+        "Investment/Financial Scams",
+        "Foreign Language Spam", # was spanish spam
+        "Housing/Urgent Account Scams",
+        "Travel/Promotion Advertising Scams",
+        "Company Information Scams",
+    ]
+
+    # Gensim NMF Model - Uni - Spam
+    uni_spam_nmf_num_topics1 = 7
+    uni_spam_nmf_model1, uni_ham_nmf_model1, uni_spam_nmf_coher1, uni_ham_nmf_coher1 = spamNMFModel(uni_spam_df, uni_ham_df, uni_spam_corpus, uni_ham_corpus, uni_spam_dictionary, uni_ham_dictionary, "tokens", uni_spam_nmf_num_topics1)
+
+    new_topics=[]
+
+    for topic_id, topic in uni_spam_nmf_model1.print_topics(num_topics=uni_spam_nmf_num_topics1, num_words=20):
+        # print(f'Topic {topic_id}: {topic}')
+        indiv_topic_dict = {}
+        indiv_topic = topic.split(" + ")
+        for ind in indiv_topic:
+            sep = ind.split("*")
+            word = sep[1][1:-1]
+            val = float(sep[0])
+            indiv_topic_dict[word] = val
+        new_topics.append(indiv_topic_dict)
+
+    print("Coherence score for spam topic model:", uni_spam_nmf_coher1)
+
+    new_text_corpus =  uni_spam_dictionary.doc2bow(inputTextDf['text'][0].split())
+    # predicted topic distribution
+    pred_topic_distri = uni_spam_nmf_model1[new_text_corpus]
+
+    max_coher = max(pred_topic_distri, key=lambda x: x[1])
+    topic_chosen_id = max_coher[0]
+    topic_coher_score = max_coher[1]
+    topic_chosen_dict = new_topics[topic_chosen_id]
+    topic_label = topic_labels_spam[topic_chosen_id]
+
+    return topic_chosen_dict, topic_label, topic_coher_score
+
+def generate_wordcloud_per_topic(topic, topic_label):
+    # Create a figure with multiple subplots, one for each topic
+    fig, axes = plt.subplots(figsize=(10, 5))
+
+    # Generate a word cloud for the current topic
+    wordcloud = WordCloud(width=800, height=400, background_color="white").generate_from_frequencies(topic)
+
+    # Plot the word cloud for the current topic
+    axes.imshow(wordcloud, interpolation='bilinear')
+    axes.set_title(topic_label)
+    axes.axis('off')
+    
+    plt.tight_layout()
+    plt.show()
