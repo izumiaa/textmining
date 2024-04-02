561bc8e7ccb53063c66e8b972fac6f903c1b0878
izumiaa <91410858+izumiaa@users.noreply.github.com>
2024-04-02 09:05:26 +0800

    update topic model integration

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 import streamlit as st
 import numpy as np
+from io import BytesIO
 
 from helper_functions.classifier import classify_text
-from helper_functions.topic_model import get_topic, generate_wordcloud_per_topic
+# from helper_functions.topic_model import get_topic, generate_wordcloud_per_topic
+from helper_functions.topic_model import get_topic
+
+
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+def generate_wordcloud_per_topic(topic, topic_label):
+    # Generate a word cloud for the current topic
+    wordcloud = WordCloud(width=800, height=400, background_color="white").generate_from_frequencies(topic)
+
+    # Plot the word cloud for the current topic
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.title(topic_label)
+    plt.axis('off')
+
+    # Save the plot to a BytesIO buffer
+    buf = BytesIO()
+    plt.savefig(buf, format="png")
+    buf.seek(0)
+
+    # Display the plot using Streamlit
+    st.image(buf)
 
 # Define the Streamlit app
 def main():
@@ -24,18 +47,20 @@ def main():
                 st.markdown("<p style='font-size: 36px; color: green;'>Not scam!</p>", unsafe_allow_html=True)
 
             # Call the get_topic function to extract the topic and coherence score
-            path = '../../data/train_data.csv'
-            topic_chosen_dict, topic_label, topic_coher_score = get_topic(path, user_input)
+            # path = './data/train_data.csv'
+            # topic_chosen_dict, topic_label, topic_coher_score = get_topic(path, user_input)
 
-            # Display the coherence score
-            st.write(f"The coherence score of your text with the topic chosen is {round(topic_coher_score, 4)}")
+            # # Display the coherence score
+            # st.write(f"The coherence score of your text with the topic chosen is {round(topic_coher_score, 4)}")
 
-            # Generate and display the word cloud
-            st.markdown("<p style='font-size: 24px; margin-top: 20px;'>Word Cloud for Chosen Topic:</p>", unsafe_allow_html=True)
-            generate_wordcloud_per_topic(topic_chosen_dict, topic_label)
+            # # Generate and display the word cloud
+            # st.markdown("<p style='font-size: 24px; margin-top: 20px;'>Word Cloud for Chosen Topic:</p>", unsafe_allow_html=True)
+            # generate_wordcloud_per_topic(topic_chosen_dict, topic_label)
         else:
             st.warning("Please enter some text.")
 
 # Run the Streamlit app
 if __name__ == "__main__":
     main()
+
+    
